35d76790a72bc80699378fc1e631706ec467d31c
郭金利 <guojinli>
2021-12-27 15:17:37 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import styles from './index.css';
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+        <li>
+          <a href="https://umijs.org/guide/getting-started.html">
+            Getting Started
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
